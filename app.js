1197b4dc44dbef05f2e5ee0138e20e91e4a54a59
Vinh Huynh <huynhtanvinhktqn123@gmail.com>
2022-12-30 11:02:49 +0700

    category admin vwCategoryAdmin folder

--- app.js
+++ app.js
@@ -209,6 +209,9 @@ app.use('/home', homeRouter);
 //Admin
 app.use('/adCategorySideBar', adCategorySideBar);
 
+//Admin
+app.use('/adCategorySideBar', adCategorySideBar);
+
 // app.get('/teacher/addCourse', (req, res) => res.render('teacher/addCourse'))
 
 app.all('*', (req, res, next) => {
